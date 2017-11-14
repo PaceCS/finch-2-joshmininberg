1265e009e3762f42182d63f30ec181dd117f3acb
gwpjp <john.pearson.02@gmail.com>
2017-11-14 14:07:33 -0500

    Updated skeleton code.

--- sound.js
+++ sound.js
@@ -7,17 +7,20 @@ let finch = new Finch();
 // -----------------------------------------------
 // Don't change anything above the line
 
-// Our first set of code will illuminate the Finch in one color, wait 1 second, then illuminate it in a different color
+// Our first set of code will play one note,, wait 1 second, then play a different note
 finch.playTone(440, 1000);
 wait(1000);
 finch.playTone(220, 1000);
 wait(1000);
 
 // Play a song
-// Write your code here to model 3 cycles of a traffic light
+// Write your code here to play a song.  The array arr has the frequencies you will need to play
+// The timing for each note is stored in the time array
 const arr = [329.6, 293.7, 261.6, 293.7, 329.6, 329.6, 329.6, 293.7, 293.7, 293.7, 329.6, 392, 392, 329.6, 293.7, 261.6, 293.7, 329.6, 329.6, 329.6, 329.6, 293.7, 293.7, 329.6, 293.7, 261.6];
+const time = [1000,1000,1000,1000,1000,1000,2000,1000,1000,2000,1000,1000,2000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000]
 
-const time = [1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+
 
 // Create a sound & light show
 // Write your own code here to create a light show of your choosing
